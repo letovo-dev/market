bfcd9fd757b09a92b7b0e8167ec81f722a33e8a2
Zkh-dot <kernzahar@gmail.com>
2025-06-19 19:48:37 +0200

    status conflict

--- transactions.cc
+++ transactions.cc
@@ -269,7 +269,7 @@ namespace transactions::server {
                     .done();
                     break;
                 case TransactionStatus::NoMoney:
-                    return req->create_response(restinio::status_not_acceptable())
+                    return req->create_response(restinio::status_conflict())
                         .append_header("Content-Type", "text/plain; charset=utf-8")
                         .set_body(Comment::giveMe().no_money)
                     .done();
